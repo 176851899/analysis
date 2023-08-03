70fb30a0e47fc7cd504d36dc3e0d32b00ab22256
renbing <renbing@ewell.cc>
2023-08-03 17:35:55 +0800

    feat: 添加xxx管理模块'

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: Ren Bing
+ * @Date: 2022-11-08 09:53:52
+ * @LastEditors: Ren Bing
+ * @LastEditTime: 2023-08-03 17:31:31
+ * @Description: 请填写简介
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,8 +14,10 @@ import  './utils/mockoff'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+setTimeout(()=>{
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+},3000)
